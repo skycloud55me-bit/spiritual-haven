9578ea86403b8e35c7360784598bf198179f56bb
skycloud55me-bit <skycloud55me@gmail.com>
2025-10-19 00:11:41 +0100

    Update Streamlit command to bind to all interfaces

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 pip install --upgrade pip setuptools
 pip install -r requirements.txt
-streamlit run enhanced_universe.py --server.port $PORT --server.enableCORS false
+streamlit run enhanced_universe.py --server.port $PORT --server.address 0.0.0.0 --server.enableCORS false
